8087fa9bbddbb7cea2c1ff72942a2e379dc22c37
Evan <94743980+xzripper@users.noreply.github.com>
2025-04-11 00:19:52 +0200

    Add files via upload

---
+++ ghost/ghost-aux-win.py
@@ -0,0 +1,4 @@
+# H17I Ghost Build System.
+# Here you can install required dependencies or do any other work required for compilation on Linux.
+
+print('[Ghost] (Windows) No auxiliary processes...')
