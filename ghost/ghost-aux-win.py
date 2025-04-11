9032644f2b9fbdd5aa92f48bada47150b5e98b59
Evan <94743980+xzripper@users.noreply.github.com>
2025-04-11 16:43:58 +0200

    Delete ghost directory

--- ghost/ghost-aux-win.py
+++
@@ -1,4 +0,0 @@
-# H17I Ghost Build System.
-# Here you can install required dependencies or do any other work required for compilation on Linux.
-
-print('[Ghost] (Windows) No auxiliary processes...')
